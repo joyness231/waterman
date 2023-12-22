d6c229331a6bffd078cd5bc9a59caa23ee7a393e
joyness231 <joy@aspireweb.com.au>
2023-12-22 09:46:24 +0800

    kahit ano

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,8 @@ function App() {
 
   return (
     <>
-     <h1 className='text-xl font-bold text-center text-orange-500' >Jello</h1>
+     <h1 className='text-xl font-bold text-center text-orange-500' >Hello</h1>
+  
     </>
   )
 }
